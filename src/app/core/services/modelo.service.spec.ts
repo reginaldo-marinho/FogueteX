976163714c8ae6ec324d141b50b8c496c7a8ce09
reginaldo <reginaldomarinho2016@gmail.com>
2022-09-03 12:20:29 -0400

    commit

---
+++ src/app/core/services/modelo.service.spec.ts
@@ -0,0 +1,9 @@
+import { ModeloService } from "./modelo.service";
+const modeloService = new ModeloService();
+
+describe("A suite", function() {
+  it("", function() {  
+    
+    expect(true).toBe(true);
+  });
+});
